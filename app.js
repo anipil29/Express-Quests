3eca9b33f00a29dd8516b9d6a4127e4149b94f51
anipil29 <fleursarts@gmail.com>
2022-12-13 14:23:15 +0100

    Express 03

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
@@ -14,10 +16,12 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
 
 const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
@@ -26,3 +30,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
--- movieHandlers.js
+++ movieHandlers.js
@@ -13,7 +13,7 @@ const getMovies = (req, res) => {
 };
 
 const getMovieById = (req, res) => {
-  const id = parseInt(req.params.id);
+const id = parseInt(req.params.id);
 
   database
     .query("select * from movies where id = ?", [id])
@@ -30,8 +30,25 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
